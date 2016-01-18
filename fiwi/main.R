071d3229cc5af5ed1041f8582dadbb37611ecacb
wojtek <wojtekkulma@gmail.com>
2016-01-18 20:28:16 +0100

    yahoo

--- fiwi/main.R
+++ fiwi/main.R
@@ -1,2 +1,15 @@
 install.packages("Rcpp")
 
+ibm_url <- "http://real-chart.finance.yahoo.com/table.csv?s=IBM&a=07&b=24&c=2010&d=07&e=24&f=2015&g=d&ignore=.csv"
+lnkd_url <- "http://real-chart.finance.yahoo.com/table.csv?s=LNKD&a=07&b=24&c=2010&d=07&e=24&f=2015&g=d&ignore=.csv"
+
+yahoo.read <- function(url){
+  dat <- read.table(url,header=TRUE,sep=",")
+  df <- dat[,c(1,5)]
+  df$Date <- as.Date(as.character(df$Date))
+  return(df)}
+
+print(yahoo.read(ibm_url)
+
+
+
